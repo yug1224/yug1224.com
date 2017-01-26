3847beb5aa8be8ee9fd0ba1c7433e31ce6005319
Yuji Yamaguchi <yuji.yamaguchi24@gmail.com>
2017-01-27 02:57:31 +0900

    update

--- src/routes/archives.js
+++ src/routes/archives.js
@@ -118,6 +118,7 @@ exports.show = (req, res) => {
         image: archive.image ? archive.image : config.twitter.image,
         url: `${config.blog.url}/archives/${archive._id}`
       };
+      data.analytics = config.GoogleAnalytics;
 
       res.status(200).render('archives_show', data);
     }
